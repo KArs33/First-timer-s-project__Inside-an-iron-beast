28f6f9e5949476e01327efdb9f6a4f70e58a7812
Kevin A. <karsenault999@gmail.com>
2025-10-29 17:36:03 -0400

    minor changes. I think debug works now?

--- driver.cpp
+++ driver.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
     std::cout<<"test: if you are reading this, main() of driver is running.\n";
         
     // Debug helper: put a marker here to set a breakpoint on the next line
-    volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
+    //volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
     std::cout << "Paused at BREAKPOINT_MARKER. Press Enter to continue (or attach debugger and continue)...\n";
     std::cin.get();
     
@@ -22,13 +22,13 @@ int main(int argc, char *argv[]){
 
     std::cout<<"test: if you are reading this, main of driver got past the first if() check.\n";
     // Debug helper: put a marker here to set a breakpoint on the next line
-    volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
+    //volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
     std::cout << "Paused at BREAKPOINT_MARKER. Press Enter to continue (or attach debugger and continue)...\n";
     std::cin.get();
 
     std::cout<<"Loading files: "<< argv[1] <<" and "<<argv[2]<<"\n";
     // Debug helper: put a marker here to set a breakpoint on the next line
-    volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
+    //volatile int BREAKPOINT_MARKER = 0; // <-- set a breakpoint on this line if you want
     std::cout << "Paused at BREAKPOINT_MARKER. Press Enter to continue (or attach debugger and continue)...\n";
     std::cin.get();
     srand(time(NULL));
